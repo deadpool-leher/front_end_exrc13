b11970da197079bae006ff5a39194f78da27ffb0
Busdan_Joshua <revildobusdan@gmail.com>
2024-10-31 09:36:31 +0700

    adding folder

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Hello from "./components/Hello";
+import World from "./components/Word";
+const App = () => {
+  return (
+    /* JSX */
+    <div>
+      <h1>Hello World</h1>
+      <Hello />
+      <World />
+    </div>
+  );
+};
+
+export default App;

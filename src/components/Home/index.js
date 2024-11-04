99b603664050a4e5e93c5266e3cfcda885b153fb
Busdan_Joshua <revildobusdan@gmail.com>
2024-11-04 22:59:29 +0700

    adding components

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Card from "../CardInfo";
+import Search from "../Search";
 
 const Home = (props) => {
   return (
     <div>
       <h1>{props.title}</h1>
       <Card />
+      <Search />
     </div>
   );
 };
